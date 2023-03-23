755b974419695d44411b599da11c095082fc7b81
Towkir Ahmed <towkir17@gmail.com>
2023-03-24 03:09:05 +0600

    added a color set for column label

--- src/mixins/common.js
+++ src/mixins/common.js
@@ -1,4 +1,30 @@
 export default {
+  data() {
+    return {
+      columnColors: [
+        '#55efc4',
+        '#81ecec',
+        '#74b9ff',
+        '#a29bfe',
+        '#dfe6e9',
+        '#00b894',
+        '#00cec9',
+        '#0984e3',
+        '#6c5ce7',
+        '#b2bec3',
+        '#ffeaa7',
+        '#fab1a0',
+        '#ff7675',
+        '#fd79a8',
+        '#636e72',
+        '#fdcb6e',
+        '#e17055',
+        '#d63031',
+        '#e84393',
+        '#2d3436',
+      ],
+    };
+  },
   computed: {
     sideBarOpen() {
       return this.$store.state.sideBarOpen;
