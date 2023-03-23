b3419ce6604c030eff8cdb86ac7783e62e4ea662
Towkir Ahmed <towkir17@gmail.com>
2023-03-24 01:42:40 +0600

    added column component

---
+++ src/components/Column.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="column">
+    <h4>{{ column.name }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Column',
+  props: {
+    column: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.column {
+  display: inline-block;
+  overflow-y: auto;
+  margin-right: 24px;
+  width: 280px;
+  height: 100%;
+  h4 {
+    @include heading-s;
+    color: $grey;
+    text-transform: uppercase;
+  }
+}
+</style>
