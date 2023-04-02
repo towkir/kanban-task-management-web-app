bb3ba3ad3d8850924842c24fd463931c10152eff
Towkir Ahmed <towkir17@gmail.com>
2023-04-03 00:59:10 +0600

    turned label colors to object items instead of strings

--- src/mixins/common.js
+++ src/mixins/common.js
@@ -2,26 +2,86 @@ export default {
   data() {
     return {
       columnColors: [
-        '#55efc4',
-        '#81ecec',
-        '#74b9ff',
-        '#a29bfe',
-        '#dfe6e9',
-        '#00b894',
-        '#00cec9',
-        '#0984e3',
-        '#6c5ce7',
-        '#b2bec3',
-        '#ffeaa7',
-        '#fab1a0',
-        '#ff7675',
-        '#fd79a8',
-        '#636e72',
-        '#fdcb6e',
-        '#e17055',
-        '#d63031',
-        '#e84393',
-        '#2d3436',
+        {
+          label: 'Color 1',
+          color: '#55efc4',
+        },
+        {
+          label: 'Color 2',
+          color: '#81ecec',
+        },
+        {
+          label: 'Color 3',
+          color: '#74b9ff',
+        },
+        {
+          label: 'Color 4',
+          color: '#a29bfe',
+        },
+        {
+          label: 'Color 5',
+          color: '#dfe6e9',
+        },
+        {
+          label: 'Color 6',
+          color: '#00b894',
+        },
+        {
+          label: 'Color 7',
+          color: '#00cec9',
+        },
+        {
+          label: 'Color 8',
+          color: '#0984e3',
+        },
+        {
+          label: 'Color 9',
+          color: '#6c5ce7',
+        },
+        {
+          label: 'Color 10',
+          color: '#b2bec3',
+        },
+        {
+          label: 'Color 11',
+          color: '#ffeaa7',
+        },
+        {
+          label: 'Color 12',
+          color: '#fab1a0',
+        },
+        {
+          label: 'Color 13',
+          color: '#ff7675',
+        },
+        {
+          label: 'Color 14',
+          color: '#fd79a8',
+        },
+        {
+          label: 'Color 15',
+          color: '#636e72',
+        },
+        {
+          label: 'Color 16',
+          color: '#fdcb6e',
+        },
+        {
+          label: 'Color 17',
+          color: '#e17055',
+        },
+        {
+          label: 'Color 18',
+          color: '#d63031',
+        },
+        {
+          label: 'Color 19',
+          color: '#e84393',
+        },
+        {
+          label: 'Color 20',
+          color: '#2d3436',
+        },
       ],
     };
   },
