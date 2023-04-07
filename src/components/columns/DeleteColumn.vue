fb79f1b56417cb6e6751fc99d817e47bb03d9581
Towkir Ahmed <towkir17@gmail.com>
2023-04-08 03:04:26 +0600

    added component for handling delete column action

---
+++ src/components/columns/DeleteColumn.vue
@@ -0,0 +1,46 @@
+<template>
+  <modal
+    id="delete-column"
+    title="Delete Column"
+    ok-text="Delete"
+    variant="danger"
+    @ok="deleteColumn"
+  >
+    <template #body>
+      <p>
+        Are you sure you want to delete the column <b>{{ columnName }}</b> ? <br>
+        This action will delete all the tasks and sub tasks in this column and can not be reversed.
+      </p>
+    </template>
+  </modal>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue';
+
+export default {
+  name: 'DeleteColumn',
+  components: { Modal },
+  props: {
+    columnToDelete: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    columnName() {
+      return this.columnToDelete.name;
+    },
+  },
+  methods: {
+    deleteColumn() {
+      this.$store.commit('deleteColumn', this.columnToDelete);
+      this.$root.$emit('modal::hide', 'delete-column');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
