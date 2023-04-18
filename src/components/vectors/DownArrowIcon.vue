be2c7036c73117f25286fb7977e48c03c68d65c7
Towkir Ahmed <towkir17@gmail.com>
2023-04-18 06:27:34 +0600

    added some more vector icon components

---
+++ src/components/vectors/DownArrowIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0.79834 1.54883L5.49682 6.24731L10.1953 1.54883" stroke="#635FC7" stroke-width="2"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'DownArrowIcon',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/vectors/PlusIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7.368 12V7.344H12V4.632H7.368V0H4.656V4.632H0V7.344H4.656V12H7.368Z" fill="white"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'PlusIcon',
+};
+</script>
+
+<style scoped>
+
+</style>
